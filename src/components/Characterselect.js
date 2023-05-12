f6baf01e46cf3545d43316debbc137cc6cecc872
Maxime <malryck33@gmail.com>
2023-05-12 21:08:10 +0200

    splash screens update with mouseover

--- src/components/Characterselect.js
+++ src/components/Characterselect.js
@@ -1,19 +1,51 @@
-import Reimu from '../characters/reimu';
-import Madeline from '../characters/madeline';
+import reimu from '../characters/reimu';
+import madeline from '../characters/madeline';
 import '../scss/characterselect.scss';
 import React from 'react';
 import vs from '../assets/img/vs.png';
 
 class Characterselect extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          player1: null,
+          player2: null,    
+        };
+    }
 
     selectChar = (element) => {
-        console.log(element.currentTarget.getAttribute('data'));
+        this.setState({
+            player1: element.currentTarget.getAttribute('data')
+        });
+    }
+
+    leftSplash = (element) => {
+
+        let output;
+
+        console.log('the input :', this.state.player1);
+        console.log('my try : ', window[this.state.player1]);
+        console.log('What I want :', reimu);
+
+        if(this.state.player1 == null)
+        {
+            output = <div>coucou</div>
+        }
+        else
+        {
+            output = <div>
+                {this.state.player1}
+                <img alt="1st player" src={[this.state.player1].selectImage}></img>
+                </div>;
+        }
+
+        return(output);
     }
 
     render(){
         const characterList = [
-            Reimu,
-            Madeline
+            reimu,
+            madeline
         ];
 
         return(
@@ -21,7 +53,7 @@ class Characterselect extends React.Component {
             <section className="cs-display">
 
                 <div className="splashContainer">
-                    <img alt="1st player"></img>
+                    {this.leftSplash()}
                 </div>
 
                 <div className="vsContainer">
@@ -29,13 +61,13 @@ class Characterselect extends React.Component {
                 </div>
 
                 <div className="splashContainer">
-                    <img alt="2bd player"></img>
+                    <img alt="2nd player"></img>
                 </div>
 
             </section>
             <ul className="cs-l">
                 {characterList.map( i => (
-                    <li data={i.devName} onClick={this.selectChar} className="cs-btn" key={i.name}>
+                    <li data={i.devName} onMouseEnter={this.selectChar} className="cs-btn" key={i.name}>
 
                     <img src={i.selectImage} alt={i.name}/>
                     <div className="cs-name">{i.name}</div>
